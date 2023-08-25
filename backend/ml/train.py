57cc408a981504c2acc1a995e60b068b6fd0f16f
marcos-c1 <marcos.b.campos14@gmail.com>
2023-08-25 02:11:23 -0300

    feat(ml): json to dataframe functions

--- backend/ml/train.py
+++ backend/ml/train.py
@@ -1,7 +1,27 @@
 import pandas as pd 
+import seaborn
+import os
 
-artists_json = pd.read_json('../output/artists_Mon Aug 21 2023 13:26:54 GMT-0300 (Brasilia Standard Time).json')
-artists_json.to_csv('./out/artists.csv');
+def get_newest_file(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
 
-df = pd.DataFrame(artists_json)
-print(df)
+def json_to_dataframe(json):
+    arr = []
+    df = pd.DataFrame()
+
+    for i in range(len(json['artists'])):
+        df = pd.DataFrame(json['artists'][i])
+        arr.append(df)
+
+    tbl = pd.concat(arr)
+    return tbl
+
+def save_dataframe_as_csv(df):
+    df.to_csv('./out/artists.csv')
+    
+if __name__ == '__main__':
+    json = pd.read_json(get_newest_file('../output/'))
+    df = json_to_dataframe(json)
+    print(df[['name', 'genres', 'followers']])
